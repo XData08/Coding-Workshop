ff0061f2ad882fe76fbf6bedfffbda269ad360e0
XData08 <71586774+XData08@users.noreply.github.com>
2021-09-04 22:20:17 +0800

    Update 7_ExtraLibraries.c

--- Day 1 : C Fundamentals/7_ExtraLibraries.c
+++ Day 1 : C Fundamentals/7_ExtraLibraries.c
@@ -5,13 +5,22 @@
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
+#include <stdio.h>      // Standard input output 
+#include <stdlib.h>     // Standard library
+#include <math.h>       // Math library
+#include <time.h>       // Time library
+#include <conio.h>      // Console input output
+#include <unistd.h>     // Unix standard
 
 int main(){
+    
+    // Random number generator
     srand((unsigned int) time(NULL));
-    int x = rand();
-    printf("%d", x);
+    int x = rand();                         // This will generate random number
+    
+   
+    
+    
+
+    return 0;
 }
